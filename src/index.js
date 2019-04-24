6e63f5ad46f7805e8aa55926a739485051539c28
Siva Theja Vyamajala <sivatheja1997@gmail.com>
2019-04-24 20:52:25 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {AppProvider} from "@shopify/polaris";
+import App from './App';
+import "@shopify/polaris/styles.css";
+
+ReactDOM.render(<AppProvider><App /></AppProvider>, document.getElementById("root"));
